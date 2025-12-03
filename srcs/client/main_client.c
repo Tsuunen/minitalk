c8cec94744accbc7692f9345db1bf2a1295ec1d5
Remi Laforgue <relaforg@z4r10p4.42lyon.fr>
2025-12-03 17:32:10 +0100

    Envoie binaire OK

--- srcs/client/main_client.c
+++ srcs/client/main_client.c
@@ -1,22 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_client.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: relaforg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 15:29:32 by relaforg          #+#    #+#             */
-/*   Updated: 2025/12/03 15:31:08 by relaforg         ###   ########.fr       */
+/*   Updated: 2025/12/03 17:30:09 by relaforg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft.h"
 
+void	send_char(char c, int pid)
+{
+	int	i;
+
+	i = 0;
+	while (c || i < 8)
+	{
+		if (c % 2)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		i++;
+		c /= 2;
+		usleep(100);
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	(void) argc;
+	int	pid;
 
-	kill(ft_atoi(argv[1]), SIGUSR1);
+	if (argc < 3)
+		return (0);
+	pid = ft_atoi(argv[1]);
+	while (*argv[2])
+	{
+		send_char(*argv[2], pid);
+		argv[2]++;
+		usleep(100);
+	}
+	// int	i = 0;
+	// while (i < 8)
+	// {
+	// 	kill(pid, SIGUSR1);
+	// 	i++;
+	// 	usleep(100);
+	// }
 	return (0);
 }
--- srcs/server/main_server.c
+++ srcs/server/main_server.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_server.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: relaforg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 15:20:01 by relaforg          #+#    #+#             */
-/*   Updated: 2025/12/03 15:29:14 by relaforg         ###   ########.fr       */
+/*   Updated: 2025/12/03 17:14:13 by relaforg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 #include <signal.h>
 #include "libft.h"
 
+void	test(int signal)
+{
+	if (signal == SIGUSR1)
+		ft_printf("0\n");
+	else if (signal == SIGUSR2)
+		ft_printf("1\n");
+}
+
 int	main(void)
 {
 	pid_t	pid;
-	sigset_t	sigset;
+	struct sigaction act;
 
-	if (sigaddset(&sigset, SIGUSR1) == -1)
-		return (1);
-	if (sigaddset(&sigset, SIGUSR2) == -1)
-	{
-		sigemptyset(&sigset);
-		return (1);
-	}
+	ft_bzero(&act, sizeof(act));
+	act.sa_handler = test;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
 	pid = getpid();
 	ft_printf("%d\n", pid);
 	while (1);
-	sigemptyset(&sigset);
 	return (0);
 }
