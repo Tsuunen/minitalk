0cdf8c43de0aef1b4cc84ecde92677adf91402e5
Remi Laforgue <relaforg@z4r3p2.42lyon.fr>
2025-12-04 14:01:07 +0100

    minitalk mandatory

--- srcs/client/main_client.c
+++ srcs/client/main_client.c
@@ -6,28 +6,23 @@
 /*   By: relaforg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 15:29:32 by relaforg          #+#    #+#             */
-/*   Updated: 2025/12/03 17:30:09 by relaforg         ###   ########.fr       */
+/*   Updated: 2025/12/04 13:25:26 by relaforg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft.h"
 
-void	send_char(char c, int pid)
+void	send_char(unsigned int c, int pid, int i)
 {
-	int	i;
-
-	i = 0;
-	while (c || i < 8)
-	{
-		if (c % 2)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		i++;
-		c /= 2;
-		usleep(100);
-	}
+	if (i == 8)
+		return ;
+	send_char(c / 2, pid, i + 1);
+	if (c % 2)
+		kill(pid, SIGUSR2);
+	else
+		kill(pid, SIGUSR1);
+	usleep(100);
 }
 
 int	main(int argc, char **argv)
@@ -37,18 +32,13 @@ int	main(int argc, char **argv)
 	if (argc < 3)
 		return (0);
 	pid = ft_atoi(argv[1]);
+	send_char(ft_strlen(argv[2]) + 1, pid, -24);
 	while (*argv[2])
 	{
-		send_char(*argv[2], pid);
+		send_char(*argv[2], pid, 0);
 		argv[2]++;
 		usleep(100);
 	}
-	// int	i = 0;
-	// while (i < 8)
-	// {
-	// 	kill(pid, SIGUSR1);
-	// 	i++;
-	// 	usleep(100);
-	// }
+	send_char(0, pid, 0);
 	return (0);
 }
--- srcs/server/main_server.c
+++ srcs/server/main_server.c
@@ -6,7 +6,7 @@
 /*   By: relaforg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 15:20:01 by relaforg          #+#    #+#             */
-/*   Updated: 2025/12/03 17:14:13 by relaforg         ###   ########.fr       */
+/*   Updated: 2025/12/04 14:00:21 by relaforg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,77 @@
 #include <signal.h>
 #include "libft.h"
 
-void	test(int signal)
+#define SIZE_BYTES 4
+
+typedef struct s_message
 {
-	if (signal == SIGUSR1)
-		ft_printf("0\n");
-	else if (signal == SIGUSR2)
-		ft_printf("1\n");
+	unsigned int	size;
+	char			*message;
+}	t_message;
+
+t_message	message;
+
+void	signal_handler(int signal)
+{
+	static unsigned int	bit_count;
+	static unsigned int	c;
+
+	c = c << 1;
+	if (signal == SIGUSR2)
+		c = c | 1;
+	bit_count++;
+	message.message[23] = 'd';
+	if (bit_count % 8 == 0)
+	{
+		if (message.size == 0 && bit_count == 8 * SIZE_BYTES)
+		{
+			message.size = c;
+			message.message = ft_calloc(message.size, sizeof(char));
+			if (!message.message)
+				exit(1);
+			c = 0;
+		}
+		else if (message.size)
+		{
+			if (!c)
+			{
+				ft_printf("%s\n", message.message);
+				message.size = 0;
+				bit_count = 0;
+				free(message.message);
+			}
+			else
+				message.message[bit_count / 8 - SIZE_BYTES - 1] = c;
+			c = 0;
+		}
+	}
+}
+
+void	clean_exit(int signal)
+{
+	if (signal == SIGSEGV)
+		ft_printf("Segmentation Fault: you noob\n");
+	if (message.message)
+		free(message.message);
+	exit(0);
 }
 
 int	main(void)
 {
 	pid_t	pid;
-	struct sigaction act;
+	struct sigaction	act;
+	struct sigaction	act_quit;
 
+	message.message = NULL;
+	message.size = 0;
 	ft_bzero(&act, sizeof(act));
-	act.sa_handler = test;
+	act.sa_handler = signal_handler;
+	ft_bzero(&act_quit, sizeof(act));
+	act_quit.sa_handler = clean_exit;
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
+	sigaction(SIGINT, &act_quit, NULL);
+	sigaction(SIGSEGV, &act_quit, NULL);
 	pid = getpid();
 	ft_printf("%d\n", pid);
 	while (1);
